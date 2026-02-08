ca0bb26d5e015a79046383cbceecba15cf819331
Wendy Gesler <wgesler@triangleexperience.com>
2026-02-08 04:32:05 -0700

    config

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -4,5 +4,5 @@ export const environment = {
     dev: false,
     local: false,
     title: 'RentAll - Stage',
-    apiUrl: 'https://stage.rentall.services/'
+    apiUrl: 'https://rentall-api-cvetchcfewgpcqb5.centralus-01.azurewebsites.net/'
 };
