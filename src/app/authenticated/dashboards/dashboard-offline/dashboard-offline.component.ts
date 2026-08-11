2318e3e8ab36ae02d89077a1b749c34e8ce50258
Wendy Gesler <w.gesler@gmail.com>
2026-08-11 13:09:16 -0600

    Region rename

--- src/app/authenticated/dashboards/dashboard-offline/dashboard-offline.component.ts
+++ src/app/authenticated/dashboards/dashboard-offline/dashboard-offline.component.ts
@@ -60,7 +60,7 @@ export class DashboardOfflineComponent implements OnInit, OnDestroy {
   }
   //#endregion
 
-  //#region Utility Methods
+  //#region Form Response Methods
   get checklistColumns(): ColumnSet {
     return {
       expand: { displayAs: ' ', maxWidth: '5ch', sort: false },
